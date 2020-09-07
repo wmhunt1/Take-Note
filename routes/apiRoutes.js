8c2df6162146d7a8d61c8a13b3f750a825dba09f
Matt Hunt <wmatthunt@gmail.com>
2020-09-07 15:59:10 -0400

    added routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,23 +4,22 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
+var db = require("../db/db");
 
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
+module.exports = function (app) {
   // API GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases when a user visits a link
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/api/tables", function(req, res) {
-    res.json(tableData);
+  app.get("/api/notes", function (req, res) {
+    res.json(db);
   });
 
   // app.get("/api/waitlist", function(req, res) {
@@ -35,17 +34,17 @@ module.exports = function(app) {
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
-  app.post("/api/tables", function(req, res) {
+  app.post("/api/notes", function (req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body parsing middleware
-    if (tableData.length < 5) {
-      tableData.push(req.body);
+    if (db.length < 5) {
+      db.push(req.body);
       res.json(true);
     }
     else {
-      waitListData.push(req.body);
-      res.json(false);
+      // waitListData.push(req.body);
+      // res.json(false);
     }
   });
 
@@ -53,7 +52,7 @@ module.exports = function(app) {
   // I added this below code so you could clear out the table while working with the functionality.
   // Don"t worry about it!
 
-  app.post("/api/clear", function(req, res) {
+  app.post("/api/clear", function (req, res) {
     // Empty out the arrays of data
     tableData.length = 0;
     waitListData.length = 0;
